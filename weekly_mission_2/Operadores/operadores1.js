ef9fc55d5488f6c330114f01f4621ce93b6d8fc4
MarcosLopez <marcoslm@ciencias.unam.mx>
2022-04-13 00:13:40 -0500

    Printing list elements

---
+++ weekly_mission_2/Operadores/operadores1.js
@@ -0,0 +1,9 @@
+// Ejemplo 1: for Each para imprimir elementos de una lista
+
+console.log("Ejemplo 1: Imprimiendo elementos de una lista");
+
+const numbers = [1, 2, 3, 4, 5];
+
+numbers.forEach(number => console.log(number));
+
+// numbers.forEach(function(number) { console.log(number); });
