63d8b19ee5f4fd4bf16abe8445207187d872fbb9
MarcosLopez <marcoslm@ciencias.unam.mx>
2022-04-13 01:43:28 -0500

    Using sort

---
+++ weekly_mission_2/Operadores/operadores15.js
@@ -0,0 +1,7 @@
+// Ejemplo 15: Uso de sort para ordenar elementos
+
+console.log("Ejemplo 15: Elementos ordenados usando el método sort");
+
+const products = ['Milk', 'Coffee', 'Sugar', 'Honey', 'Apple', 'Carrot'];
+console.log(products.sort());
+
