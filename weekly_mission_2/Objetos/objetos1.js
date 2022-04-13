29b2870cc2c1ee6546270770461f9030d5da0fd5
MarcosLopez <marcoslm@ciencias.unam.mx>
2022-04-12 23:28:20 -0500

    Creating empty object

---
+++ weekly_mission_2/Objetos/objetos1.js
@@ -0,0 +1,7 @@
+// Ejemplo 1: Crear un objeto vacío 
+console.log("Ejemplo 1: Crear un objeto vacío");
+const objeto1 = {};
+const objeto2 = new Object();
+
+console.log(objeto1);
+console.log(objeto2);
