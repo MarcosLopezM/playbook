be9df94e8a3ded618593538b8ff297d02b6ac999
MarcosLopez <marcoslm@ciencias.unam.mx>
2022-04-13 00:24:11 -0500

    Prints each element in uppercase

---
+++ weekly_mission_2/Operadores/operadores3.js
@@ -0,0 +1,6 @@
+// Ejemplo 3: forEach para imprimir los países en letras mayúsculas
+
+console.log("Ejemplo 3: Imprimiendo la lista de países en mayúsculas");
+
+const countries = ["Finland", "Denmark", "Sweden", "Norway", "Iceland"];
+countries.forEach(country => console.log(country.toUpperCase()));
