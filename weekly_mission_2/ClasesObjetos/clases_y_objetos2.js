aabd5cbb8cfb77bc70f540107184f1e7440d29cc
MarcosLopez <marcoslm@ciencias.unam.mx>
2022-04-13 14:03:06 -0500

    Creating object from empty class

---
+++ weekly_mission_2/ClasesObjetos/clases_y_objetos2.js
@@ -0,0 +1,9 @@
+// Ejemplo 2: Crear un objet a partir de una clase vacía
+
+console.log("Ejemplo 2: Crear un objet a partir de una clase vacía");
+
+class Pet {}
+
+const myPet = new Pet(); // Creando una instancia de la clase
+
+console.log(myPet);
