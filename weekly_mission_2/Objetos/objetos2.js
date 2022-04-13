09a5c959cbbb0d91d0d75854b2903a89a9658deb
MarcosLopez <marcoslm@ciencias.unam.mx>
2022-04-12 23:44:31 -0500

    Creating object w/ properties

---
+++ weekly_mission_2/Objetos/objetos2.js
@@ -0,0 +1,9 @@
+// Ejemplo 2: Crear un objeto con propiedades
+
+console.log("Ejemplo 2: Crear un objeto con propiedades");
+const objeto1 = {
+    name: "Marcos",
+    age: 22
+};
+
+console.log(objeto1);
