b9258d0b40eb99be7191f5cd66308d96bba13544
MarcosLopez <marcoslm@ciencias.unam.mx>
2022-04-15 10:58:58 -0500

    Example: Fixed typo and added new objects

--- weekly_mission_2/Exercises/Exercise1/example.js
+++ weekly_mission_2/Exercises/Exercise1/example.js
@@ -17,10 +17,53 @@ const repository = {
         return this.issuesOpen + this.issuesClosed;
     },
     getGeneralInfo: function () {
-        return `This repository ${this.name} was create by ${this.author} and it has ${this.stars} stars.`;
+        return `This repository ${this.name} was created by ${this.author} and it has ${this.stars} stars.`;
     }
 }
 
 console.log(`Nombre del repositorio: ${repository.name}`);
 console.log(`Issues totales: ${repository.getTotalIssues()}`);
 console.log(repository.getGeneralInfo());
+
+const issue = {
+    title: "Issue 1",
+    repositoryNameAssociated: "MissionNodeJS",
+    status: "Open",
+    numberOfComments: 2,
+    labels: ["bug", "performance"],
+    author: "MarcosLopezM",
+    dateCreated: "14/04/2022",
+    lastUpdated: "14/04/2022",
+
+    getTitleAndName: function () {
+        return `The issue ${this.title} was created by ${this.author}.`;
+    },
+
+    getGeneralInfo: function () {
+        return `The issue was ${this.status} in the repository ${this.repositoryNameAssociated} with labels ${this.labels[0]} and ${this.labels[1]}.`;
+    }    
+}
+
+console.log(`This issue was created on ${issue.dateCreated} and last updated on ${issue.lastUpdated}`);
+console.log(issue.getTitleAndName());
+console.log(issue.getGeneralInfo());
+
+const pullRequest = {
+    title: "First Pull Request",
+    branchName: "main",
+    dateCreated: "14/04/2022",
+    status: "Open",
+    repositoryNameAssociated: "MissionNodeJS",
+
+    getStatus: function () {
+        return `The pull request's status is ${this.status}.`;
+    },
+
+    getTitleAndAuthor: function () {
+        return `The pull request ${this.title} was created by ${this.author}.`;
+    }
+}
+
+console.log(`This pull request was created on ${pullRequest.dateCreated}.`);
+console.log(pullRequest.getStatus());
+console.log(pullRequest.getTitleAndAuthor());
